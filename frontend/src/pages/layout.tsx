fed27a9697b9c225a9b49d9bfb4c4a014a5095c9
louis <louis.chedru@epitech.eu>
2025-10-06 20:32:23 +0200

    [FIX] remove footer

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
 
 export default function Layout() {
   return (
     <>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
