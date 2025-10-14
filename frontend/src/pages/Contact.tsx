a463123f850451c4e6726ab8c04c01af4dd8e07f
louis <louis.chedru@epitech.eu>
2025-10-14 15:09:47 +0200

    [ADD] Message sent confirmation popup

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -41,6 +41,7 @@ function ContactForm() {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
   const [email, setEmail] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
   const submit = async () => {
     try {
@@ -59,63 +60,87 @@ function ContactForm() {
   };
 
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={e => {
-          e.preventDefault();
-          submit();
-        }}
-        className="flex flex-col gap-5"
-      >
-        <div className="gap-5 flex flex-col lg:flex-row">
-          <div className="lg:w-1/2">
-            <TextInput
-              name="Nom"
-              id="contact-nom"
-              value={name}
-              onChange={e => setName(e.currentTarget.value)}
+    <>
+      {isOpen && <MessageSentPopup setIsOpen={setIsOpen} />}
+      <div>
+        <form
+          action=""
+          onSubmit={e => {
+            e.preventDefault();
+            const doStuff = async () => {
+              await submit();
+              setIsOpen(true);
+            };
+            doStuff();
+          }}
+          className="flex flex-col gap-5"
+        >
+          <div className="gap-5 flex flex-col lg:flex-row">
+            <div className="lg:w-1/2">
+              <TextInput
+                name="Nom"
+                id="contact-nom"
+                value={name}
+                onChange={e => setName(e.currentTarget.value)}
+              />
+            </div>
+            <div className="lg:w-1/2">
+              <TextInput
+                name="Prénomprenom"
+                id="contact-nom"
+                value={firstname}
+                onChange={e => setFirstname(e.currentTarget.value)}
+              />
+            </div>
+          </div>
+          <div className="lg:flex lg:gap-5">
+            <div className="lg:w-1/2">
+              <TextInput
+                name="Adresse email"
+                id="contact-email"
+                value={email}
+                onChange={e => setEmail(e.currentTarget.value)}
+              />
+            </div>
+            <div className="lg:w-1/2">
+              <TextInput
+                name="Objet"
+                id="contact-objet"
+                value={subject}
+                onChange={e => setSubject(e.currentTarget.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <TextArea
+              name="Message"
+              id="contact-message"
+              value={message}
+              onChange={e => setMessage(e.currentTarget.value)}
             />
           </div>
-          <div className="lg:w-1/2">
-            <TextInput
-              name="Prénomprenom"
-              id="contact-nom"
-              value={firstname}
-              onChange={e => setFirstname(e.currentTarget.value)}
-            />
+          <div className="flex justify-center">
+            <SubmitInput />
           </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+function MessageSentPopup({ setIsOpen }: { setIsOpen: (b: boolean) => void }) {
+  return (
+    <div className="fixed w-screen h-screen bg-black bg-opacity-50 top-0 left-0 z-50">
+      <div className="h-screen w-screen p-5 flex items-center md:p-20 lg:p-28">
+        <div className="bg-white w-full p-5 md:p-10">
+          <p className="text-center font-black text-2xl mb-5 md:text-3xl md:mb-20">
+            Votre message a bien été envoyé !
+          </p>
+          <button className="hover:underline" onClick={() => setIsOpen(false)}>
+            Fermer
+          </button>
         </div>
-        <div className="lg:flex lg:gap-5">
-          <div className="lg:w-1/2">
-            <TextInput
-              name="Adresse email"
-              id="contact-email"
-              value={email}
-              onChange={e => setEmail(e.currentTarget.value)}
-            />
-          </div>
-          <div className="lg:w-1/2">
-            <TextInput
-              name="Objet"
-              id="contact-objet"
-              value={subject}
-              onChange={e => setSubject(e.currentTarget.value)}
-            />
-          </div>
-        </div>
-        <div>
-          <TextArea
-            name="Message"
-            id="contact-message"
-            value={message}
-            onChange={e => setMessage(e.currentTarget.value)}
-          />
-        </div>
-        <div className="flex justify-center">
-          <SubmitInput />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
